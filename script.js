1de290099eb9154fbd503353161a8eb0f3153c10
dharshan <dharshanmuzic@gmail.com>
2025-09-12 07:50:49 +0530

    updated_code

---
+++ script.js
@@ -0,0 +1,7 @@
+function send(){
+    var input = document.getElementById("msgFeild");
+    var inputValue = input.value;
+    console.log(inputValue);
+    input.value = "";
+}
+// document.getElementsByClassName("userMsg").innerHtml
